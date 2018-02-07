a892d57d130820448017ac56fa36d0a78e4abcbf
Julie Groth <groth.julie@gmail.com>
2018-02-06 20:34:32 -0800

    updated conditionals for 'movie-this'

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,33 +1,39 @@
 require("dotenv").config();
 var keysFile = require('./keys.js');
 // var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+// var twitterClient = new Twitter(keys.twitter);
 var request = require("request");
+var nodeArgs = process.argv;
+var movieName = "";
+var requestType = process.argv[2];
 
 // * `my-tweets`
+
+
 
 // * `spotify-this-song`
 
 // * `movie-this`
-var nodeArgs = process.argv;
-var movieName = "";
 
-if (nodeArgs.length > 2) {
-for (var i = 2; i < nodeArgs.length; i++) {
-    if (i > 2 && i < nodeArgs.length) {
-        movieName = movieName + "+" + nodeArgs[i];
-    } else {
-        movieName += nodeArgs[i];
+// if 3rd process.argv is "movie-this" and a movie title is provided, define movieName with entered movie title
+if (requestType == "movie-this" && nodeArgs.length > 3) {
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+            movieName = movieName + "+" + nodeArgs[i];
+        } else {
+            movieName += nodeArgs[i];
+        }
     }
-}
-} else {
+    // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
+} else if (requestType == "movie-this" && nodeArgs.length <= 3) {
     movieName = "Mr.+Nobody";
 };
 
 var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-console.log(movieUrl);
+// console.log(movieUrl);
 
-request(movieUrl, function(error, response, body) {
+// send request to omdb api, if no errors and response is returned, parse and show specific responses
+request(movieUrl, function (error, response, body) {
     if (!error && response.statusCode === 200) {
         console.log("Movie Title: " + JSON.parse(body).Title);
         console.log("Year Released: " + JSON.parse(body).Year);
