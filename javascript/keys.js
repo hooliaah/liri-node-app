05d3fbfe699cda425f4725eb106e7f83f82f5364
Julie Groth <groth.julie@gmail.com>
2018-02-06 21:38:27 -0800

    added twitter code but not working properly - auth problems

--- javascript/keys.js
+++ javascript/keys.js
@@ -1,4 +1,4 @@
-exports.twitter = {
+exports.twitterKey = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
--- javascript/liri.js
+++ javascript/liri.js
@@ -1,14 +1,29 @@
+// global variables - general
 require("dotenv").config();
 var keysFile = require('./keys.js');
-// var spotify = new Spotify(keys.spotify);
-// var twitterClient = new Twitter(keys.twitter);
+// var spotifyClient = new Spotify(keysFile.spotify);
 var request = require("request");
 var nodeArgs = process.argv;
-var movieName = "";
 var requestType = process.argv[2];
 
-// * `my-tweets`
+// twitter specific global variables
+var Twitter = require('twitter');
+var client = new Twitter(keysFile.twitterKey);
+var params = {screen_name: 'jukiegroth'};
 
+// OMDB specific global variables
+var movieName = "";
+
+// * `my-tweets`
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//     if (!error) {
+//         console.log(tweets);
+//         console.log(response);
+//         console.log("request sent to twitter");
+//     } else {
+//         console.log("there was an error" + JSON.stringify(error));
+//     }
+// });
 
 
 // * `spotify-this-song`
@@ -16,33 +31,35 @@ var requestType = process.argv[2];
 // * `movie-this`
 
 // if 3rd process.argv is "movie-this" and a movie title is provided, define movieName with entered movie title
-if (requestType == "movie-this" && nodeArgs.length > 3) {
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            movieName = movieName + "+" + nodeArgs[i];
-        } else {
-            movieName += nodeArgs[i];
-        }
-    }
-    // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
-} else if (requestType == "movie-this" && nodeArgs.length <= 3) {
-    movieName = "Mr.+Nobody";
-};
+// if (requestType == "movie-this" && nodeArgs.length > 3) {
+//     for (var i = 3; i < nodeArgs.length; i++) {
+//         if (i > 3 && i < nodeArgs.length) {
+//             movieName = movieName + "+" + nodeArgs[i];
+//         } else {
+//             movieName += nodeArgs[i];
+//         }
+//     }
+//     // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
+// } else if (requestType == "movie-this" && nodeArgs.length <= 3) {
+//     movieName = "Mr.+Nobody";
+// };
 
-var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// console.log(movieUrl);
+// var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// // console.log(movieUrl);
 
-// send request to omdb api, if no errors and response is returned, parse and show specific responses
-request(movieUrl, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        console.log("Movie Title: " + JSON.parse(body).Title);
-        console.log("Year Released: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-        console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-        console.log("Language: " + JSON.parse(body).Language);
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log("Actors: " + JSON.parse(body).Actors);
-    }
-})
+// // send request to omdb api, if no errors and response is returned, parse and show specific responses
+// request(movieUrl, function (error, response, body) {
+//     if (!error && response.statusCode === 200) {
+//         console.log("Movie Title: " + JSON.parse(body).Title);
+//         console.log("Year Released: " + JSON.parse(body).Year);
+//         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+//         console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+//         console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+//         console.log("Language: " + JSON.parse(body).Language);
+//         console.log("Plot: " + JSON.parse(body).Plot);
+//         console.log("Actors: " + JSON.parse(body).Actors);
+//     } else {
+//         console.log("There was an error");
+//     }
+// })
 // * `do-what-it-says`
