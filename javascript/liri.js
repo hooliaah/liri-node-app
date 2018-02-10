9f5a362e99d13378545bda6bcbf514b60b0da066
Julie Groth <groth.julie@gmail.com>
2018-02-09 23:22:45 -0800

    uncommented everything

--- javascript/liri.js
+++ javascript/liri.js
@@ -19,17 +19,17 @@ var movieName = "";
 
 // * `my-tweets`
 // if 3rd process.argv == "my-tweets", console date and content of my last 20 tweets
-// if (requestType == "my-tweets") {
-//     client.get('statuses/user_timeline', params, function (error, tweets, response) {
-//         if (!error) {
-//             for (var i = 0; i < tweets.length; i++) {
-//                 console.log(tweets[i].created_at + ": " + tweets[i].text);
-//             }
-//         } else {
-//             console.log("there was an error" + JSON.stringify(error));
-//         }
-//     });
-// };
+if (requestType == "my-tweets") {
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at + ": " + tweets[i].text);
+            }
+        } else {
+            console.log("there was an error" + JSON.stringify(error));
+        }
+    });
+};
 
 // * `spotify-this-song`
 var songName = "";
@@ -42,60 +42,59 @@ if (requestType == "spotify-this-song" && nodeArgs.length > 3) {
         }
     }
 } else if (requestType == "spotify-this-song" && nodeArgs.length <= 3) {
-     songName = "The Sign Ace of Base";
- };
+    songName = "The Sign Ace of Base";
+};
 
-    spotify.search({
-        type: 'track',
-        query: songName,
-        limit: 1
-    },
-        function (error, data) {
-            if (error) {
-                return console.log('Error occurred: ' + error);
-            }
-            // Artist(s)
-            console.log("Artist name: " + data.tracks.items[0].artists[0].name);
-            // The song's name
-            console.log("Song name: " + data.tracks.items[0].name);
-            // A preview link of the song from Spotify
-            console.log("Song URL: " + data.tracks.items[0].external_urls.spotify);
-            // The album that the song is from
-            console.log("Album name: " + data.tracks.items[0].album.name);
-        });
+spotify.search({
+    type: 'track',
+    query: songName,
+    limit: 1
+},
+    function (error, data) {
+        if (error) {
+            return console.log('Error occurred: ' + error);
+        }
+        // Artist(s)
+        console.log("Artist name: " + data.tracks.items[0].artists[0].name);
+        // The song's name
+        console.log("Song name: " + data.tracks.items[0].name);
+        // A preview link of the song from Spotify
+        console.log("Song URL: " + data.tracks.items[0].external_urls.spotify);
+        // The album that the song is from
+        console.log("Album name: " + data.tracks.items[0].album.name);
+    });
 
 // * `movie-this`
 
 // if 3rd process.argv is "movie-this" and a movie title is provided, define movieName with entered movie title
-// if (requestType == "movie-this" && nodeArgs.length > 3) {
-//     for (var i = 3; i < nodeArgs.length; i++) {
-//         if (i > 3 && i < nodeArgs.length) {
-//             movieName = movieName + "+" + nodeArgs[i];
-//         } else {
-//             movieName += nodeArgs[i];
-//         }
-//     }
-//     // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
-// } else if (requestType == "movie-this" && nodeArgs.length <= 3) {
-//     movieName = "Mr.+Nobody";
-// };
+if (requestType == "movie-this" && nodeArgs.length > 3) {
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+            movieName = movieName + "+" + nodeArgs[i];
+        } else {
+            movieName += nodeArgs[i];
+        }
+    }
+    // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
+} else if (requestType == "movie-this" && nodeArgs.length <= 3) {
+    movieName = "Mr.+Nobody";
+};
 
-// var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// // console.log(movieUrl);
+var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// // send request to omdb api, if no errors and response is returned, parse and show specific responses
-// request(movieUrl, function (error, response, body) {
-//     if (!error && response.statusCode === 200) {
-//         console.log("Movie Title: " + JSON.parse(body).Title);
-//         console.log("Year Released: " + JSON.parse(body).Year);
-//         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-//         console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-//         console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-//         console.log("Language: " + JSON.parse(body).Language);
-//         console.log("Plot: " + JSON.parse(body).Plot);
-//         console.log("Actors: " + JSON.parse(body).Actors);
-//     } else {
-//         console.log("There was an error");
-//     }
-// })
+// send request to omdb api, if no errors and response is returned, parse and show specific responses
+request(movieUrl, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log("Movie Title: " + JSON.parse(body).Title);
+        console.log("Year Released: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+    } else {
+        console.log("There was an error");
+    }
+})
 // * `do-what-it-says`
