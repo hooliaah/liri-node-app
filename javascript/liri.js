367a046d4b09b6803c437bf28df4bccf999803e6
Julie Groth <groth.julie@gmail.com>
2018-02-11 23:27:37 -0800

    added searchRandom functionality

--- javascript/liri.js
+++ javascript/liri.js
@@ -14,6 +14,10 @@ var params = { screen_name: 'jukiegroth' };
 // Spotify global variables
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var songName = "";
+
+// OMDB variable
+var movieName = "";
 
 switch (requestType) {
     case "my-tweets":
@@ -21,39 +25,51 @@ switch (requestType) {
         break;
 
     case "spotify-this-song":
-        var songName = "";
-        if (nodeArgs.length > 3) {
-            for (var i = 3; i < nodeArgs.length; i++) {
-                if (i > 3 && i < nodeArgs.length) {
-                    songName = songName + "+" + nodeArgs[i];
-                } else {
-                    songName += nodeArgs[i];
-                }
-            }
-            spotifySearch();
-        } else if (nodeArgs.length <= 3) {
-            songName = "The Sign Ace of Base";
-            spotifySearch();
-        };
+        spotifySearch();
         break;
 
     case "movie-this":
-    var movieName = "";
-        if (nodeArgs.length > 3) {
-            for (var i = 3; i < nodeArgs.length; i++) {
-                if (i > 3 && i < nodeArgs.length) {
-                    movieName = movieName + "+" + nodeArgs[i];
-                } else {
-                    movieName += nodeArgs[i];
-                }
-            } movieSearch();
-            // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
-        } else if (nodeArgs.length <= 3) {
-            movieName = "Mr.+Nobody";
-            movieSearch();
-        };
+        movieSearch();
+        break;
+
+    case "do-what-it-says":
+        randomSearch();
+        break;
+
+    default:
+        console.log("Please enter a valid request. Search types include: my-tweets, spotify-this-song, movie-this, or do-what-it-says.")
         break;
 };
+
+// function to read random.txt and execute appropriate function based on the text content
+function randomSearch() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            console.log("There was an error: " + error);
+        }
+        var commandSeparator = data.indexOf(",");
+        var commandArray = [];
+        var searchTerm = [];
+        for (var i = 0; i < commandSeparator; i++) {
+            commandArray.push(data[i]);
+        };
+        for (var j = commandSeparator + 1; j < data.length; j++) {
+            searchTerm.push(data[j]);
+        };
+        var joinCommand = commandArray.join("");
+        var joinSearchTerm = searchTerm.join("");
+        if (joinCommand == "my-tweets") {
+            twitterSearch();
+        } else if (joinCommand == "spotify-this-song") {
+            songName = joinSearchTerm;
+            spotifySearch();
+        } else if (joinCommand == "movie-this") {
+            movieName = joinSearchTerm;
+            movieSearch();
+        }
+    })
+};
+
 
 // function to search for 20 latest tweets from JukieGroth on Twitter
 function twitterSearch() {
@@ -66,10 +82,23 @@ function twitterSearch() {
             console.log("there was an error" + JSON.stringify(error));
         }
     });
-}
+};
 
 // function to search Spotify for track name
 function spotifySearch() {
+    if (nodeArgs.length > 3) {
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                songName = songName + "+" + nodeArgs[i];
+            } else {
+                songName += nodeArgs[i];
+            }
+        }
+    } else if (requestType == "do-what-it-says") {
+        songName = songName;
+    } else if (nodeArgs.length <= 3) {
+        songName = "The Sign Ace of Base";
+    };
     spotify.search({
         type: 'track',
         query: songName,
@@ -84,10 +113,25 @@ function spotifySearch() {
             console.log("Song URL: " + data.tracks.items[0].external_urls.spotify);
             console.log("Album name: " + data.tracks.items[0].album.name);
         });
-}
+};
 
 // function to search OMDB for movie title
 function movieSearch() {
+    if (nodeArgs.length > 3) {
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                movieName = movieName + "+" + nodeArgs[i];
+            } else {
+                movieName += nodeArgs[i];
+            }
+        }
+        // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
+    } else if (requestType == "do-what-it-says") {
+        movieName = movieName;
+    } else if (nodeArgs.length <= 3) {
+        movieName = "Mr.+Nobody";
+    };
+
     var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
     // send request to omdb api, if no errors and response is returned, parse and show specific responses
     request(movieUrl, function (error, response, body) {
@@ -104,4 +148,4 @@ function movieSearch() {
             console.log("There was an error");
         }
     })
-}
+};
