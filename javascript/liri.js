a5eb2f88fcf8d6614f3f6fb512e7d81715a63517
Julie Groth <groth.julie@gmail.com>
2018-02-11 22:44:53 -0800

    created switch case statement

--- javascript/liri.js
+++ javascript/liri.js
@@ -15,16 +15,48 @@ var params = { screen_name: 'jukiegroth' };
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-// OMDB global variables
-var movieName = "";
+switch (requestType) {
+    case "my-tweets":
+        twitterSearch();
+        break;
 
-// * `my-tweets`
-// if 3rd process.argv == "my-tweets", console date and content of my last 20 tweets
-if (requestType == "my-tweets") {
-    twitterSearch();
+    case "spotify-this-song":
+        var songName = "";
+        if (nodeArgs.length > 3) {
+            for (var i = 3; i < nodeArgs.length; i++) {
+                if (i > 3 && i < nodeArgs.length) {
+                    songName = songName + "+" + nodeArgs[i];
+                } else {
+                    songName += nodeArgs[i];
+                }
+            }
+            spotifySearch();
+        } else if (nodeArgs.length <= 3) {
+            songName = "The Sign Ace of Base";
+            spotifySearch();
+        };
+        break;
+
+    case "movie-this":
+    var movieName = "";
+        if (nodeArgs.length > 3) {
+            for (var i = 3; i < nodeArgs.length; i++) {
+                if (i > 3 && i < nodeArgs.length) {
+                    movieName = movieName + "+" + nodeArgs[i];
+                } else {
+                    movieName += nodeArgs[i];
+                }
+            } movieSearch();
+            // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
+        } else if (nodeArgs.length <= 3) {
+            movieName = "Mr.+Nobody";
+            movieSearch();
+        };
+        break;
 };
 
-function twitterSearch(){
+// function to search for 20 latest tweets from JukieGroth on Twitter
+function twitterSearch() {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             for (var i = 0; i < tweets.length; i++) {
@@ -36,22 +68,7 @@ function twitterSearch(){
     });
 }
 
-// * `spotify-this-song`
-var songName = "";
-if (requestType == "spotify-this-song" && nodeArgs.length > 3) {
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            songName = songName + "+" + nodeArgs[i];
-        } else {
-            songName += nodeArgs[i];
-        }
-    }
-    spotifySearch();
-} else if (requestType == "spotify-this-song" && nodeArgs.length <= 3) {
-    songName = "The Sign Ace of Base";
-    spotifySearch();
-};
-
+// function to search Spotify for track name
 function spotifySearch() {
     spotify.search({
         type: 'track',
@@ -62,34 +79,14 @@ function spotifySearch() {
             if (error) {
                 return console.log('Error occurred: ' + error);
             }
-            // Artist(s)
             console.log("Artist name: " + data.tracks.items[0].artists[0].name);
-            // The song's name
             console.log("Song name: " + data.tracks.items[0].name);
-            // A preview link of the song from Spotify
             console.log("Song URL: " + data.tracks.items[0].external_urls.spotify);
-            // The album that the song is from
             console.log("Album name: " + data.tracks.items[0].album.name);
         });
 }
 
-// * `movie-this`
-
-// if 3rd process.argv is "movie-this" and a movie title is provided, define movieName with entered movie title
-if (requestType == "movie-this" && nodeArgs.length > 3) {
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            movieName = movieName + "+" + nodeArgs[i];
-        } else {
-            movieName += nodeArgs[i];
-        }
-    } movieSearch();
-    // if 3rd process.argv is "movie-this" and a movie title is NOT provided, define movieName as "Mr.+Nobody"
-} else if (requestType == "movie-this" && nodeArgs.length <= 3) {
-    movieName = "Mr.+Nobody";
-    movieSearch();
-};
-
+// function to search OMDB for movie title
 function movieSearch() {
     var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
     // send request to omdb api, if no errors and response is returned, parse and show specific responses
@@ -108,5 +105,3 @@ function movieSearch() {
         }
     })
 }
-
-// * `do-what-it-says`
